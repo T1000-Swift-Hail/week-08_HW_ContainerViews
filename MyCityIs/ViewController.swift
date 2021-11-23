cf017e417f6a64d9952d4abf8947eb4f7c9fc0a6
hanaalmutlak <Hana.Khamis.has.s@tuwaiq.edu.sa>
2021-11-23 17:40:08 +0300

    Done

---
+++ MyCityIs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCityIs
+//
+//  Created by Monafh on 18/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
