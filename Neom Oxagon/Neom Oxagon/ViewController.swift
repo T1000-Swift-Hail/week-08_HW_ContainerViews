aa1474ea3fb9cf8e46cc8ecf708370dd2f81412e
Aryaf AlQabali <Aryaf.Aqeel.has.s@tuwaiq.edu.sa>
2021-11-23 14:51:44 +0300

    Done HW Neom Oxagon

---
+++ Neom Oxagon/Neom Oxagon/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Neom Oxagon
+//
+//  Created by AryafAlaqabali on 18/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imgNeomOxagon: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   //     imgNeomOxagon.image = UIImage (named: "neom2")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
