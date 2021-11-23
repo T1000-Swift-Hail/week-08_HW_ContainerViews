598a7232b2c387d417ae41e6be4b3567acbeb36b
Tlal <talalalglaadi@MacBook-Air-tlal.local>
2021-11-23 15:30:00 +0300

    Container Done

---
+++ Contaner/Contaner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contaner
+//
+//  Created by طلال عبيدالله دعيع القلادي on 23/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
