b50fd4e3d4fb1afdb5612d3462b79ce7ce7a4af0
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-11-23 16:12:10 +0300

    Done Hw

---
+++ Hw Riyadh City/Hw Riyadh City/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hw Riyadh City
+//
+//  Created by HIND12 on 18/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
